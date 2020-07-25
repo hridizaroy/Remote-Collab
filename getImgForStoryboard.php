b5374f8b74e4c3b93aaeae55f44dd7ca6ddb904b
hridizaroy <52705197+hridizaroy@users.noreply.github.com>
2020-07-26 00:14:15 +0530

    Update getImgForStoryboard.php

--- getImgForStoryboard.php
+++ getImgForStoryboard.php
@@ -1,22 +1,10 @@
 <?php
-//Done: Choose a random integer in range of no.of images to choose the image
-//Make a temp file to store the game data in (Image + Search string (replaced with spaces))
-//The file is opened from the server for the respective people
+
 include 'genWord.php';
 $keywords = "";
 function getImg() {
     global $keywords, $textSplit, $punctuations, $stopWords;
     $keywords = generateWord($textSplit, $punctuations, $stopWords);
-
-  /*  function getWord() {
-
-        global $keywords;
-        //ob = Output Buffering
-        ob_start(); // begin collecting output
-        include 'genWord.php';
-        $word = ob_get_clean();
-        $keywords.=$word;
-    }*/
 
     for ($i = 0; $i < 3; $i++) {
         $keywords.='+'.generateWord($textSplit, $punctuations, $stopWords);
